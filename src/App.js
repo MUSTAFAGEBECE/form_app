99e5d2e4d192f68b3f3c11b69002dd41f5687907
MUSTAFAGEBECE <mustafagbc003@gmail.com>
2023-07-17 00:03:51 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
+import React, {useState} from "react"
 import './App.css';
 
 function App() {
+  const [contact,setContact]=useState({
+    fName:"",
+    lName:"",
+    email:""
+  })
+  function handleChange(event){
+    const {name,value}=event.target
+    setContact(prevValue=>{
+      return{
+        ...prevValue,
+        [name]:value
+      }
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Hello {contact.fName}{contact.lName}</h1>
+      <p>{contact.email}</p>
+      <form>
+        <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+        <input
+          onChange={handleChange}
+          name="lName"
+          value={contact.lName}
+          placeholder="Last Name"
+        />
+        <input
+          onChange={handleChange}
+          name="email"
+          value={contact.email}
+          placeholder="Email"
+        />
+        <button>Submit</button>
+      </form>
     </div>
   );
 }
 
 export default App;
+
+
+/*
+const [contact,setContact]=useState({
+    fName:"",
+    lName:"",
+    email:""
+  })
+  function handleChange(event){
+    const {name,value}=event.target
+    setContact(prevValue=>{
+      return{
+        ...prevValue,
+        [name]:value
+      }})}
+  <input
+          onChange={handleChange}
+          name="fName"
+          value={contact.fName}
+          placeholder="First Name"
+        />
+*/
